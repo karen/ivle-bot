e54c82c336827c1fc835837006885c245a05e5cb
Karen Ang <thekarenang@gmail.com>
2016-07-31 14:44:12 +1000

    Add html stripper for announcements

---
+++ html_stripper.py
@@ -0,0 +1,18 @@
+from html.parser import HTMLParser
+
+class HTMLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = HTMLStripper()
+    s.feed(html)
+    return s.get_data()
